83fb4ca55a2db58b77d022c2e72df8224fb47ff6
renzo <renvmorales@gmail.com>
2017-11-05 10:27:43 -0200

    Convert all labels to numeric before pipeline and add extra comments.

--- naiveBayes.py
+++ naiveBayes.py
@@ -19,9 +19,20 @@ df = pd.read_csv('SMS.csv', encoding = "ISO-8859-1")
 print('\nReading csv file ....')
 
 
-
+# shuffle data
 X, Y = shuffle(df['Full_Text'], df['IsSpam'])
 
+
+
+# function to convert into numerical labels
+def binary_class(x):
+	if x=='yes':
+		return 1
+	else:
+		return 0
+
+# convert all labels to 0 or 1
+Y = Y.apply(binary_class)
 
 
 
@@ -35,27 +46,25 @@ pipeline = Pipeline([
 
 
 
-def binary_class(x):
-	if x=='yes':
-		return 1
-	else:
-		return 0
-
-
+# define the number for K-fold cross validation
 kfold=10
 
 print('\nApplying %d-fold cross-validation ...' % kfold)
 print('\n')
 
 
-scores = cross_val_score(pipeline, X, Y.apply(binary_class), 
+
+# estimate f1-score using kfold cross validation 
+f1_scores = cross_val_score(pipeline, X, Y, 
 	cv=kfold, scoring='f1')
 
-print('F1-score: %.3f (+/- %.3f)' % (scores.mean(), 2*scores.std()))
+print('F1-score: %.3f (+/- %.3f)' % (f1_scores.mean(), 
+	2*f1_scores.std()))
 
 
 
-precisions = cross_val_score(pipeline, X, Y.apply(binary_class), 
+# estimate precision using kfold cross validation 
+precisions = cross_val_score(pipeline, X, Y, 
 	cv=kfold, scoring='precision')
 
 print('Precision: %.3f (+/- %.3f)' % (precisions.mean(), 
@@ -63,7 +72,8 @@ print('Precision: %.3f (+/- %.3f)' % (precisions.mean(),
 
 
 
-recalls = cross_val_score(pipeline, X, Y.apply(binary_class), 
+# estimate recall using kfold cross validation 
+recalls = cross_val_score(pipeline, X, Y, 
 	cv=kfold, scoring='recall')
 
 print('Recall: %.3f (+/- %.3f)' % (recalls.mean(), 
